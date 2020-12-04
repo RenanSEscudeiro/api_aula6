2ddde995787cd0d9aac62372a8956ffc5c16f85b
Renan <renanescudeiro1@gmail.com>
2020-12-04 16:56:09 -0300

    Começando API aula 3

--- App.js
+++ App.js
@@ -2,12 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Navigations
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation /stack';  
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+// Páginas
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+const Autenticado = () => {
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Logout" component={Logout} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
